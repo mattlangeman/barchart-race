27d96787ea59da16b0c46d7b2211f5c5eab286e9
Matt Langeman <matt@langeman.net>
2023-07-11 13:44:16 -0400

    partially working bar chart race

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,1 +1,43 @@
 export const ssr = false;
+import * as d3 from "d3";
+
+
+function rank(names, value) {
+    const data = Array.from(names, name => ({name, value: value(name)}));
+    data.sort((a, b) => d3.descending(a.value, b.value));
+    for (let i = 0; i < data.length; ++i) data[i].rank = i;
+    return data;
+}
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+    let data = await d3.csv("/data/category-brands.csv", d3.autoType)
+    let names = new Set(data.map(d => d.name))
+    let datevalues = Array.from(d3.rollup(data, ([d]) => d.value, d => +d.date, d => d.name))
+  						.map(([date, data]) => [new Date(date), data])
+  						.sort(([a], [b]) => d3.ascending(a, b))
+
+    let keyframes = [];
+    let ka, a, kb, b;
+    let interpolationValue = 10
+    // we want to interpolate between each date in order to smooth out the animation
+    for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
+        for (let i = 0; i < interpolationValue; ++i) {
+            const t = i / interpolationValue;
+            keyframes.push([
+                new Date(ka * (1 - t) + kb * t),
+                rank(names, name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
+            ]);
+        }
+    }
+    keyframes.push([new Date(kb), rank(names, name => b.get(name) || 0)]);
+    let nameframes = d3.groups(keyframes.flatMap(([, data]) => data), d => d.name)
+
+    return {
+        data,
+        names,
+        datevalues,
+        nameframes,
+        keyframes
+    };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,15 @@
 <script>
 	import * as d3 from "d3";
+    import { fade } from 'svelte/transition';
+    import numeral from 'numeral';
 
-	let data = []
+    /** @type {import('./$types').PageData} */
+	export let data
 
-	d3.csv("/data/category-brands.csv", d3.autoType)
-		.then(res => {
-			data = res
-		})
-
-    let duration = 250
+    console.log('data', data)
+    let duration = 200
 	let barSize = 48
 	let numBars = 12
-	let interpolationValue = 10
 
 	let margin = {
 		top: 16,
@@ -23,82 +21,68 @@
     let height = margin.top + barSize * numBars + margin.bottom
     let width = 910
 
-    $: names = new Set(data.map(d => d.name))
-	$: datevalues = Array.from(d3.rollup(data, ([d]) => d.value, d => +d.date, d => d.name))
-  						.map(([date, data]) => [new Date(date), data])
-  						.sort(([a], [b]) => d3.ascending(a, b))
-
-	function rank(value) {
-  		const data = Array.from(names, name => ({name, value: value(name)}));
-  		data.sort((a, b) => d3.descending(a.value, b.value));
-  		for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(numBars, i);
-  		return data;
-	}
-
-    $: {
-        let tmpRank = rank(name => datevalues[0][1].get(name))
-        console.log('tmpRank', tmpRank)
-    }
-
-    let keyframes = []
-    $: {
-        const tmpKeyframes = [];
-        let ka, a, kb, b;
-        for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
-            for (let i = 0; i < interpolationValue; ++i) {
-            const t = i / interpolationValue;
-            tmpKeyframes.push([
-                new Date(ka * (1 - t) + kb * t),
-                rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
-            ]);
-            }
-        }
-        tmpKeyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
-        keyframes = tmpKeyframes
-    }
-
-    $: nameframes = d3.groups(keyframes.flatMap(([, data]) => data), d => d.name)
-
-	$: prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
-	$: next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
-
-	$: console.log('keyframes', keyframes)
-
-
-	$: currentDate = keyframes[0][0]
-	$: currentData = keyframes[0][1]
-
-	$: console.log('currentDate', currentDate)
-	$: console.log('currentData', currentData)
-
+	//$: prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
+	//$: next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
 
     const y = d3.scaleBand()
         .domain(d3.range(numBars + 1))
         .rangeRound([margin.top, margin.top + barSize * (numBars + 1 + 0.1)])
         .padding(0.1)
 
-    $: barData = data.slice(0, numBars).sort((a, b) => d3.descending(a.value, b.value))
-    $: console.log('barData', barData)
+    let currentKeyframe = data.keyframes[0]
+
+    $: currentKeyframeDate = currentKeyframe[0]
+    $: currentKeyframeData = currentKeyframe[1]
+
+    let barData = []
+    let currentYear = 2000
+
+    for (let i=0; i < numBars; i++) {
+        barData.push({
+            name: data.keyframes[0][1][i].name,
+            value: data.keyframes[0][1][i].value,
+            rank: data.keyframes[0][1][i].rank
+        })
+    }
+
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+
+    async function playRace() {
+        for (const keyframe of data.keyframes) {
+            currentYear = keyframe[0]
+            let tmpbarData = keyframe[1].slice(0, numBars)
+            for (let i=0; i < numBars; i++) {
+                barData[i].name = tmpbarData[i].name
+                barData[i].value = tmpbarData[i].value
+            }
+            await timer(duration)
+            /*
+            const transition = svg.transition()
+                .duration(duration)
+                .ease(d3.easeLinear);
+            barData = keyframe[1].slice(0, numBars)
+            invalidation.then(() => svg.interrupt());
+            await transition.end();
+            */
+        }
+    }
+
+    playRace()
+
+    //$: barData = data.keyframes[190][1].slice(0, numBars)
     $: domain = [0, d3.max(barData, d => d.value) || 1]
     $: xScale = d3.scaleLinear(domain, [margin.left, width - margin.right])
 
-
-    $: console.log('y', y)
-    $: console.log('xScale', xScale)
-
-    $: {
-        console.log('x0', xScale(0))
-        console.log('x1', xScale(1))
-    }
-
-
     $: ticks = xScale.ticks(width / 160)
+
 
 </script>
 
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <h1>Bar Chart Race</h1>
+    <p>This chart animates the value (in $M) of the top global brands from 2000 to 2019.</p>
+    <p></p>
+</div>
 
 {#if barData}
 <svg viewBox="0 0 {width} {height}">
@@ -115,7 +99,7 @@
     <g tranform="translate(0, {margin.top})">
         {#each ticks as x, i}
             {#if i > 0}
-            <g transform="translate({xScale(x)}, 0)">
+            <g class="ticks" style="transform:translate({xScale(x)}px, 0px)">
                 <line
                     y2="{height}"
                     stroke="white"
@@ -124,7 +108,7 @@
                     y="10"
                     text-anchor="middle"
                     dominant-baseline="hanging">
-                    {x}
+                    { numeral(x).format('0,0') }
                 </text>
             </g>
             {/if}
@@ -132,35 +116,49 @@
     </g>
     <g text-anchor="end">
         {#each barData as d, i}
-            <text
-                transform="translate({xScale(d.value)-xScale(0)}, {y(i)})"
+            <text class="bar-label"
+                style="transform:translate({xScale(d.value)-xScale(0)}px, {y(i)}px)"
                 x="-6"
                 y="21.5"
                 dy="-0.25em">
                 {d.name}
-                <tspan fill-opacity="0.7" font-weight="normal" x="-6" dy="1.15em">{d.value}</tspan>
+                <tspan fill-opacity="0.7" font-weight="normal" x="-6" dy="1.15em">{ numeral(d.value).format('0,0') }</tspan>
             </text>
         {/each}
     </g>
+    <text class="ticker"
+        text-anchor="end"
+        x="{width - 6}"
+        y="{height - 6}"
+        dy="0.0em"
+        >
+        {currentYear.getUTCFullYear()}
+    </text>
 </svg>
 {/if}
 
-<svg viewBox="0 0 {width} {height}">
-    <g fill-opacity="0.6">
-        <rect fill="#f28e2c" height="43" x="0" y="21" width="904"></rect>
-        <rect fill="#f28e2c" height="43" x="0" y="69" width="647.250276424708"></rect>
-        <rect fill="#f28e2c" height="43" x="0" y="117" width="483.4241315568154"></rect>
-        <rect fill="#f28e2c" height="43" x="0" y="165" width="420.0703036616135"></rect>
-        <rect fill="#4e79a7" height="43" x="0" y="213" width="244.54284262789176"></rect>
-        <rect fill="#f28e2c" height="43" x="0" y="261" width="235.79387041551223"></rect>
-        <rect fill="#59a14f" height="43" x="0" y="309" width="217.06867713167208"></rect>
-        <rect fill="#59a14f" height="43" x="0" y="357" width="196.17457234216042"></rect>
-        <rect fill="#af7aa1" height="43" x="0" y="405" width="175.06434825670996"></rect>
-        <rect fill="#edc949" height="43" x="0" y="453" width="171.16648238352806"></rect>
-        <rect fill="#59a14f" height="43" x="0" y="501" width="159.92827899471058"></rect>
-        <rect fill="#e15759" height="43" x="0" y="549" width="155.8413087375822"></rect>
-    </g>
-    <g tranform="translate(0, {margin.top})">
+<style>
+    rect {
+        transition: width 250ms ease-in-out;
+    }
 
-    </g>
-</svg>
+    g.ticks {
+        transition: transform 250ms ease-in-out;
+    }
+
+    text {
+        font-size: 12px;
+        font-family: sans-serif;
+        fill: #333;
+    }
+
+    text.bar-label {
+        transition: transform 250ms ease-in-out;
+    }
+
+    text.ticker {
+        font-size: 40px;
+        font-family: sans-serif;
+        fill: #333;
+    }
+</style>
